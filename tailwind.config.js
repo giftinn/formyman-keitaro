250e42126a5ea9051c2f3822a12dfefcacb8cd27
giftin.id <calistanafisaa@gmail.com>
2025-12-07 03:53:56 +0700

    Remove public index.html from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./public/index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
