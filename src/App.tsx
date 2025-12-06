4fa02c91e050304422e682d49e810be7f32e14c6
giftin.id <calistanafisaa@gmail.com>
2025-12-07 03:36:08 +0700

    Change Router to HashRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Password } from './pages/Password/Password';
 import { Video } from './pages/Video/Video';
@@ -9,22 +9,11 @@ function App() {
   return (
     <Router>
       <Routes>
-
-        {/* HALAMAN PERTAMA = WHATSAPP CHAT */}
         <Route path="/" element={<Home />} />
-
-        {/* Password */}
         <Route path="/password" element={<Password />} />
-
-        {/* Video */}
         <Route path="/video" element={<Video />} />
-
-        {/* Gallery */}
         <Route path="/gallery" element={<Gallery />} />
-
-        {/* Redirect fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
-
       </Routes>
     </Router>
   );
