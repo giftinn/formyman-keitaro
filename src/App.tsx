f5120e2906b5fc19c44b3174133637cd481d6d90
giftin.id <calistanafisaa@gmail.com>
2025-12-07 03:34:16 +0700

    Fix import paths for page components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Home } from '/pages/Home/Home';
-import { Password } from '/pages/Password/Password';
-import { Video } from '/pages/Video/Video';
-import { Gallery } from '/pages/Gallery/Gallery';
+import { Home } from './pages/Home/Home';
+import { Password } from './pages/Password/Password';
+import { Video } from './pages/Video/Video';
+import { Gallery } from './pages/Gallery/Gallery';
 
 function App() {
   return (
