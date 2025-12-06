d77c4d8d4dbfe6f0c1f21b00f71c6e19f4201914
giftin.id <calistanafisaa@gmail.com>
2025-12-07 03:32:07 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,27 @@ import { Home } from './pages/Home/Home';
 import { Password } from './pages/Password/Password';
 import { Video } from './pages/Video/Video';
 import { Gallery } from './pages/Gallery/Gallery';
-import { LoveConfetti } from './components/LoveConfetti';
 
 function App() {
   return (
     <Router>
-      <LoveConfetti />
       <Routes>
 
-        {/* Halaman pertama = Password Page */}
-        <Route path="/" element={<Password />} />
+        {/* HALAMAN PERTAMA = WHATSAPP CHAT */}
+        <Route path="/" element={<Home />} />
 
-        {/* Halaman Home kamu pindah ke /home */}
-        <Route path="/home" element={<Home />} />
+        {/* Password */}
+        <Route path="/password" element={<Password />} />
 
+        {/* Video */}
         <Route path="/video" element={<Video />} />
+
+        {/* Gallery */}
         <Route path="/gallery" element={<Gallery />} />
 
-        {/* Redirect semua yg gak dikenal */}
+        {/* Redirect fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
+
       </Routes>
     </Router>
   );
